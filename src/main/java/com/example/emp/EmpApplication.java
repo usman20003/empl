fea7a121af3b214468cf8dc9742f027a19849b10
usman20003 <usman.angadi786@gmail.com>
2024-05-22 21:38:21 +0530

    intial message

---
+++ src/main/java/com/example/emp/EmpApplication.java
@@ -0,0 +1,16 @@
+package com.example.emp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpApplication.class, args);
+		System.out.println("Application running");
+		
+		
+	}
+
+}
